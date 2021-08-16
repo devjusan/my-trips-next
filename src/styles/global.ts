7997bb86d5a2ae29a06493d2cc59ecc3e5b21058
Junior Santos <jnrplay@hotmail.com>
2021-08-16 16:20:02 -0300

    feat: all initial changes and first push

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --highligth: #e10e8d;
+    --background: #030518;
+    --white: #eee;
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -16,8 +28,20 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: var(--background);
+    color: var(--white);
   }
+
+  p {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highligth);
+  }
+
 `
 
 export default GlobalStyles
