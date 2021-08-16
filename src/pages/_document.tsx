d52a6846f6453955ece793b51be5af47c39d7c47
Junior Santos <jnrplay@hotmail.com>
2021-08-16 16:21:33 -0300

    feat: new edit to readmemd

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, {
   Head,
   Main,
   NextScript,
-  DocumentContext
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+  DocumentContext,
+} from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
+            sheet.collectStyles(<App {...props} />),
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       }
     } finally {
       sheet.seal()
