5a7f3d2e9dd1c82c3714b268aee07ed6c4ec2052
Junior Santos <jnrplay@hotmail.com>
2021-08-21 23:05:24 -0300

    feat: page About and your template

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,23 @@
+import { CloseOutline } from "@styled-icons/evaicons-outline"
+import LinkWrapper from "components/link-wrapper"
+import React from "react"
+import * as S from "./styles"
+
+const AboutTemplate = () => {
+  return (
+    <S.Content>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+      <S.Heading>My Trips</S.Heading>
+      <S.Body>
+        <p>
+          orem oasdoaj sdomas odwmd oiamsod
+          nakjlnioanjsiwoidejaosndaosmdoajsdoijawoidnaiosdnmaosndoia
+        </p>
+      </S.Body>
+    </S.Content>
+  )
+}
+
+export default AboutTemplate
